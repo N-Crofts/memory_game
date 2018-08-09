43df5ce37614dca36f160c9073d329eb4786df20
Nicholas Crofts <nicholas.crofts@gmail.com>
2018-08-09 15:30:04 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -2,22 +2,23 @@ console.log("Up and running!");
 
 var cards = ["queen", "queen", "king", "king"]; 
 
-var cardsInPlay = [];	{ 
+var cardsInPlay = [];
 
-	var cardOne = cards[0];
-	var cardTwo = cards[2];
+var flipCard = function(cardsId) {
+	console.log("User flipped " + cards[cardsId]);
+	cardsInPlay.push(cards[cardsId]);
+}
 
-	cardsInPlay.push(cardOne);
-	cardsInPlay.push(cardTwo);
+flipCard(0);
+flipCard(2);
 
-	if (cardsInPlay[0] === cardsInPlay[2])	{
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1])	{
 		alert("You've found a match!");
 	} else {
 		alert("Sorry, try again");
 	}
 }
 
-
-console.log("User flipped " + cardsInPlay);
-
+checkForMatch();
 
